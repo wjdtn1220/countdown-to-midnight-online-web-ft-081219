07295fdc053cdbf71f5d3ae24a13f0271ffc5db0
Jeong Park <wjdtn1220@gmail.com>
2019-08-15 20:34:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,17 @@
-#write your code here
+#write your code heredef countdown(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
+def countdown_with_sleep(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR"
+end
 
