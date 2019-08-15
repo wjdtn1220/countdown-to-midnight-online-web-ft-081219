936810029b74208440b86944dfeb1dec68d966b8
Jeong Park <wjdtn1220@gmail.com>
2019-08-15 20:34:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,3 @@
 #write your code here
 
-def countdown(num)
-  while num > 0
-    puts "#{num} SECOND(S)!"
-    num -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
 
-def countdown_with_sleep(num)
-  while num > 0
-    puts "#{num} SECOND(S)!"
-    num -= 1
-    sleep(1)
-  end
-  "HAPPY NEW YEAR"
-end
